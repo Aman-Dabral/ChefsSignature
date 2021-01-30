5471ca5c76f530c5d22ae1a575a2ee7960b43c96
Aman-Dabral <66616295+Aman-Dabral@users.noreply.github.com>
2021-01-30 18:35:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ if(req.url.indexOf("/login/") == 0) {
   const [email, pass, ...garb] = req.url.replace("/login/", "").split('/');
   let isFound = true;
   res.writeHead(200, {"content-type": "text/html"});
-  for(let i = 0; i < jfjfjrr.length; i++){
-    if (jfjfjrr[i].email == email && jfjfjrr[i].password == pass) {res.end("{\"login\": true}"); isFound = true;}
+  for(let superIndex = 0; superIndex < jfjfjrr.length; superIndex++){
+    if (jfjfjrr[superIndex].email == email && jfjfjrr[superIndex].password == pass) {res.end("{\"login\": true}"); isFound = true;}
   }
-   if(isFound) res.end(jfjfjrr[i].email + " " + jfjfjrr[i].pass);
+   if(isFound) res.end(jfjfjrr[superIndex].email + " " + jfjfjrr[superIndex].password);
 } else if (req.url === "/chefssignature-menu" || req.url === "/chefssignature-menu/") {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(readFileSync('./views/menu.html').toString().replace("{navbar}", readFileSync('./components/navbar.html').toString().replace("{imgurl}", "Chef's Signature")));
