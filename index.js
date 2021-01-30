3732f97e5faa9661460efa7d4f7a3a7ef3048425
Aman-Dabral <66616295+Aman-Dabral@users.noreply.github.com>
2021-01-30 18:45:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if(req.url.indexOf("/login/") == 0) {
   for(let superIndex = 0; superIndex < jfjfjrr.length; superIndex++){
     if (jfjfjrr[superIndex].email == email && jfjfjrr[superIndex].password == pass) {res.end("{\"login\": true}"); isFound = false;}
   }
-   if(isFound) {res.end(jfjfjrr.length.toString());}
+   if(isFound) {res.end(isFound.toString());}
 } else if (req.url === "/chefssignature-menu" || req.url === "/chefssignature-menu/") {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(readFileSync('./views/menu.html').toString().replace("{navbar}", readFileSync('./components/navbar.html').toString().replace("{imgurl}", "Chef's Signature")));
